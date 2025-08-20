b3b65574a5b0d7a87bd2dd03f63c5dd4d10c8e6b
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2025-08-20 22:23:32 +0530

    initial commit src: https://www.sartiano.it/marco/diigo/System%20Design/Grokking%20the%20Object%20Oriented%20Design%20Interview.html

---
+++ src/main/java/dev/bishnoi/library_management_system/Main.java
@@ -0,0 +1,7 @@
+package dev.bishnoi.library_management_system;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/dev/bishnoi/parking_lot/Main.java
@@ -0,0 +1,8 @@
+package dev.bishnoi.parking_lot;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
